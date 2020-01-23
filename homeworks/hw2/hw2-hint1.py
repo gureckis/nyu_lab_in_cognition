87caae338a03b233553826cb166580477442648b
Todd Gureckis <gureckis@mac.com>
2020-01-23 11:12:42 -0500

    start off semester fresh

--- homeworks/hw2/hw2-hint1.py
+++
@@ -1,6 +0,0 @@
-# the types of animals are a list and entry 1 
-# appears to be cats based on the name field of the dictionary it contains
-print(animal_types['types'][1]['name'])
-
-# so...
-animal_types['types'][1]['colors']
